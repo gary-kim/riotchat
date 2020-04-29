3855fcbaeeacce23661b8afe69f9afe01563e524
Gary Kim <gary@garykim.dev>
2020-04-29 13:36:36 +0800

    Set title using Riot's title

--- src/main.js
+++ src/main.js
@@ -2,6 +2,26 @@ import { generateUrl } from '@nextcloud/router';
 
 document.addEventListener('DOMContentLoaded', main);
 
+let originalTitle = "";
+let iframe;
+
 function main () {
-    document.getElementById('riot-iframe').src = generateUrl('/apps/riotchat/riot/');
+    originalTitle = document.title;
+
+    iframe = document.getElementById('riot-iframe');
+    iframe.src = generateUrl('/apps/riotchat/riot/');
+    iframe.onload = onIframeLoad;
+}
+
+function onIframeLoad () {
+    const titleObserver = new MutationObserver(setTitle);
+    titleObserver.observe(iframe.contentWindow.document.querySelector('title'), {
+        childList: true,
+        attributes: true,
+        characterData: true,
+    });
+}
+
+function setTitle () {
+    document.title = iframe.contentWindow.document.title + " - " + originalTitle;
 }
