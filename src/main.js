48116481fb962aaada8833be4d1e9c128158e5e4
Gary Kim <gary@garykim.dev>
2020-07-01 10:07:46 +0800

    Simply hash syncing
    Signed-off-by: Gary Kim <gary@garykim.dev>

--- src/main.js
+++ src/main.js
@@ -27,8 +27,6 @@ document.addEventListener('DOMContentLoaded', main);
 
 let originalTitle = "";
 let iframe;
-let ignoreNextIframeHashChange = false;
-let ignoreNextWindowHashChange = false;
 
 function main () {
     originalTitle = document.title;
@@ -53,22 +51,16 @@ function onIframeLoad () {
 
     iframe.contentWindow.onhashchange = iframeHashChanged;
     window.onhashchange = () => {
-        if (ignoreNextWindowHashChange) {
-            ignoreNextWindowHashChange = false;
-            return;
+        if (iframe.contentWindow.location.hash !== window.location.hash) {
+            iframe.contentWindow.location.hash = window.location.hash;
         }
-        ignoreNextIframeHashChange = true;
-        iframe.contentWindow.location.hash = window.location.hash;
     };
 }
 
 function iframeHashChanged () {
-    if (ignoreNextIframeHashChange) {
-        ignoreNextIframeHashChange = false;
-        return;
+    if (window.location.hash !== iframe.contentWindow.location.hash) {
+        window.location.hash = iframe.contentWindow.location.hash;
     }
-    ignoreNextWindowHashChange = true;
-    window.location.hash = iframe.contentWindow.location.hash;
 }
 
 function setTitle () {
